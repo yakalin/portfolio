6a689f1be0a72ab7ae82f71609d9747561fd46f5
Yusuf Akalin <ytakalin01@gmail.com>
2024-02-17 16:26:57 +0100

    service for projects and featured project on home page

---
+++ src/app/_services/projects.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Project } from '../_models/Project';
+import { Tag } from '../_models/Tag';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectsService {
+  projects: Project[] = [
+    {id: 0, name: 'Sample Project', summary: 'This is my portfolio website.!', description: 'This is a website I made to showcase my projects and skills.', link: '', tags: [Tag.ANGULAR, Tag.R, Tag.ELASTICSEARCH], img: ['assets/img/portfolio.png']},
+    {id: 1, name: 'Sample Project 2', summary: 'This is a different project', description: 'This is a website I made to showcase my projects and skills.', link: '', tags: [Tag.SQL, Tag.JAVA, Tag.CSS], img: ['assets/img/portfolio.png']},
+    {id: 2, name: 'Sample Project 3', summary: 'This is another project', description: 'This is a website I made to showcase my projects and skills.', link: '', tags: [Tag.HTML, Tag.PYTHON, Tag.FASTAPI], img: ['assets/img/portfolio.png']},
+    {id: 3, name: 'Sample Project 3', summary: 'This is another project', description: 'This is a website I made to showcase my projects and skills.', link: '', tags: [Tag.HTML, Tag.PYTHON, Tag.FASTAPI], img: ['assets/img/portfolio.png']},
+    {id: 4, name: 'Sample Project 3', summary: 'This is another project', description: 'This is a website I made to showcase my projects and skills.', link: '', tags: [Tag.HTML, Tag.PYTHON, Tag.FASTAPI], img: ['assets/img/portfolio.png']},
+    {id: 5, name: 'Sample Project 3', summary: 'This is another project', description: 'This is a website I made to showcase my projects and skills.', link: '', tags: [Tag.HTML, Tag.PYTHON, Tag.FASTAPI], img: ['assets/img/portfolio.png']},
+    {id: 6, name: 'Sample Project 3', summary: 'This is another project', description: 'This is a website I made to showcase my projects and skills.', link: '', tags: [Tag.HTML, Tag.PYTHON, Tag.FASTAPI], img: ['assets/img/portfolio.png']},
+    {id: 7, name: 'Sample Project 3', summary: 'This is another project', description: 'This is a website I made to showcase my projects and skills.', link: '', tags: [Tag.HTML, Tag.PYTHON, Tag.FASTAPI], img: ['assets/img/portfolio.png']}
+  ];
+
+  constructor() { }
+
+  GetProjects(): Project[] {
+    return this.projects;
+  }
+  
+  GetProjectById(id: number): Project {
+    let project = this.projects.find(project => project.id === id);
+    if (project === undefined) {
+      throw new Error('Project not found');
+    }
+    return project;
+  }
+}
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,24 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Project } from '../_models/Project';
 import { Tag } from '../_models/Tag';
+import { ProjectsService } from '../_services/projects.service';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
-export class PortfolioComponent {
-  project: Project = {
-    id: 0,
-    name: 'Sample Project',
-    summary: 'This is my portfolio website.!',
-    description: 'This is a website I made to showcase my projects and skills.',
-    link: '',
-    tags: [Tag.ANGULAR, Tag.R, Tag.ELASTICSEARCH],
-    img: ['assets/img/portfolio.png']
-  };
-  constructor(private titleService: Title) {
+export class PortfolioComponent implements OnInit{
+  projects = {} as Project[];
+
+  constructor(private titleService: Title, private projectsService: ProjectsService) {
     this.titleService.setTitle('Yusuf Akalin - Portfolio');
+  }
+
+  ngOnInit(): void {
+      this.projects = this.projectsService.GetProjects();
   }
 }
